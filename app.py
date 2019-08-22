bcc6713f159aff304c24fe45e3a0d992a6c81cb8
Ubuntu <ubuntu@ip-172-31-94-135.ec2.internal>
2019-08-22 04:23:03 +0000

    Structured skeleton for coding and uploaded html templates for use.

--- app.py
+++ app.py
@@ -37,43 +37,82 @@ PHOTOS = 'images'
 conn = pymongo.MongoClient(MONGO_URI)
 db = conn[DATABASE_NAME]
 
-@app.route('/')
-def index():
-    results = db[ALBUMS].find({})
-    # print(dumps(results))
-    return render_template("index.html", data = results)
+
 
 @app.route('/photos')
 def photos():
     results = db[PHOTOS].find({})
     return render_template("photos.html", data = results)    
 
-@app.route('/photos/upload', methods=['POST'])
-def upload():
+
+
+# Funtion to render mainpage
+@app.route('/')
+def index():
+    results = db[ALBUMS].find({})
+    # print(dumps(results))
+    return render_template("index.html", data = results)
+
+# Function to view photos in selected album
+@app.route('/albums/<album name>')
+def create_album():
+    results = db[ALBUMS].find({})
+    results1 = db[PHOTOS].find({})
+    return render_template("create_album.html", data = results)  
+
+# Function to upload photos to album
+@app.route('/albums/<album name>/upload')
+def upload_photo():
     image = request.files.get('image')
     filename = images_upload_set.save(image)
     # Get file name and file extension
     filename_raw, file_extension = os.path.splitext(TOP_LEVEL_DIR + upload_dir + filename)
-    
     filesize = os.path.getsize(TOP_LEVEL_DIR + upload_dir + filename)
     caption = request.form.get('caption')
     tags = request.form.get('tags')
+    user_selected_file = str(request.form.getlist('selection'))
     db[PHOTOS].insert({
         'image_url' : images_upload_set.url(filename),
         'image_name' : filename, 
         'image_caption' : caption,
         'image_tags' : tags,
         'uploaded_on' : timestamp(),
-        'deleted': 0,
+        'deleted': user_selected_file,
         'deleted_on' : "null",
         # Converts filesize to mb 3sf
         'file_size' : round((filesize/1000000),3),
-        
         'file_type' : file_extension,
         })
     return redirect(url_for('photos'))
-    
-    
+
+# Function to delete album 
+# Soft Delete (0 = not deleted, 1 = deleted)
+@app.route('/albums/delete/')
+def delete_album():
+    return
+
+# Function to delete photos from album
+# Soft Delete (0 = not deleted, 1 = deleted)
+@app.route('/albums/<album name>/delete/')
+def delete_photo():
+    return
+
+# Function to edit album fields
+@app.route('/albums/<album name>/edit/')
+def edit_album_field():
+    return
+
+# Funtion to move photo between album
+@app.route('/albums/<album name>/move/')
+def move_photo():
+    return
+
+# Function to edit photo fields
+@app.route('/albums/<album name>/<photo id>edit/')
+def edit_photo_field():
+    return
+
+
 # "magic code" -- boilerplate
 if __name__ == '__main__':
     app.secret_key = 'super secret key'
@@ -82,4 +121,3 @@ if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
-
